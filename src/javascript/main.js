8e2a430f13b732c09226f4439e4997c0d112ad18
isu <60576750+isuzak@users.noreply.github.com>
2024-03-22 07:54:58 +0900

    add typescript

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -5,8 +5,14 @@ import '../stylesheets/main.scss';
 import { createApp } from "vue";
 import VueApp from './VueApp.vue';
 
+import add from './add.ts';
+
+
+
 const vueApp = createApp(VueApp);
 vueApp.mount('#vue-root');
+
+console.log(add(3, 9));
 
 console.log('webpack');
 
